0cd7f0197b91b03c32f43008bfbf7c4b7587d7d7
GB-Navarro <devnavarro06@gmail.com>
2022-02-03 15:11:38 -0300

    Versão com layout dinâmico

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,21 @@
 let numeroDeCartas = 0
+let container = document.querySelector(".cardsContainer");
+
 do{
-    numeroDeCartas = prompt("Com quantas cartas você quer jogar ?");
+    variavelDeControle = prompt("Com quantas cartas você quer jogar ?");
+    if(parseInt(variavelDeControle) > 14 || parseInt(variavelDeControle) <= 0){
+        alert("O número máximo de cartas possível é 14 e o número mínimo de cartas é 2, tente novamente")
+        /*Quando cai nessa opção, o while para. Corrigir isso*/
+    }else{
+        numeroDeCartas = parseInt(variavelDeControle);
+    }
 }while(numeroDeCartas%2 != 0);
 
+function adicionarCartas(){
+    for(let contador = 0; contador < numeroDeCartas; contador ++){
+        container.innerHTML += 
+        '<div class="box"> <img src="Imagens/front.png"/> </div>';
+    }
+}
+
+adicionarCartas();
