fb80967a90346632833ebd268b2afe6725992afe
GB-Navarro <devnavarro06@gmail.com>
2022-02-04 10:25:15 -0300

    Versão com problemas no JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,21 +1,42 @@
 let numeroDeCartas = 0
-let container = document.querySelector(".cardsContainer");
+let cardsContainer = document.querySelector(".cardsContainer");
+let arrayCartasTraseiras = ['<div><img src="Imagens/bobrossparrot.gif"/></div>','<div><img src="Imagens/explodyparrot.gif"/></div>','<div><img src="Imagens/fiestaparrot.gif"/></div>','<div><img src="Imagens/metalparrot.gif"/></div>','<div><img src="Imagens/revertitparrot.gif"/></div>','<div><img src="Imagens/tripletsparrot.gif"/></div>','<div><img src="Imagens/unicornparrot.gif"/></div>','<div><img src="Imagens/fiestaparrot.gif"/></div>','<div><img src="Imagens/explodyparrot.gif"/></div>','<div><img src="Imagens/tripletsparrot.gif"/></div>','<div><img src="Imagens/bobrossparrot.gif"/></div>','<div><img src="Imagens/revertitparrot.gif"/></div>','<div><img src="Imagens/unicornparrot.gif"/></div>','<div><img src="Imagens/metalparrot.gif"/></div>'];
+let boxArray = document.querySelectorAll(".box");
 
 do{
     variavelDeControle = prompt("Com quantas cartas você quer jogar ?");
     if(parseInt(variavelDeControle) > 14 || parseInt(variavelDeControle) <= 0){
         alert("O número máximo de cartas possível é 14 e o número mínimo de cartas é 2, tente novamente")
-        /*Quando cai nessa opção, o while para. Corrigir isso*/
+        /*Ajustar o break que ocorre quando o if é satisfeito*/
     }else{
         numeroDeCartas = parseInt(variavelDeControle);
     }
 }while(numeroDeCartas%2 != 0);
 
-function adicionarCartas(){
+function adicionarCartasFrontais(){
     for(let contador = 0; contador < numeroDeCartas; contador ++){
-        container.innerHTML += 
-        '<div class="box"> <img src="Imagens/front.png"/> </div>';
+        cardsContainer.innerHTML += 
+        '<div class="box" onclick="adicionarCartasTraseiras()"> <img src="Imagens/front.png"/> </div>';
     }
 }
 
-adicionarCartas();
+adicionarCartasFrontais();
+
+function embaralharCartasTraseiras(){
+    return Math.random() - 0.5; 
+}
+arrayCartasTraseiras.sort(embaralharCartasTraseiras);
+
+function virarAsCartas(){
+    for(let i = 0; i < boxArray.length; i++){
+        boxArray[i].innerHTML = arrayCartasTraseiras[i];
+    }
+}
+function adicionarCartasTraseiras(){
+    for(let i = 0; i < boxArray.length; i++){
+        boxArray[i].addEventListener("click",virarAsCartas())
+    }
+}
+
+
+adicionarCartasTraseiras();
