7078c7dfc53ef3f57081bc5678ef027515a386b1
GB-Navarro <devnavarro06@gmail.com>
2022-02-06 15:17:11 -0300

    V 1.0

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,7 @@
 let numeroDeCartas = 0
 let main = document.querySelector("main");
 let arrayCartasTraseiras = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif','bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif','metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif'];
-let frontFace = document.querySelector(".front");
-let backFace = document.querySelector(".back");
+
 do{
     variavelDeControle = prompt("Com quantas cartas você quer jogar ?");
     if(parseInt(variavelDeControle) > 14 || parseInt(variavelDeControle) <= 0){
@@ -22,14 +21,18 @@ function adicionarCartasFrontais(){
     for(let i = 0; i < numeroDeCartas; i ++){
         main.innerHTML += 
         `<div class="cardsContainer">
-            <div class="face front-face" onclick"virarCartas()">
+            <div class="face frontFace" onclick"virarCartas()">
                 <img src="Imagens/front.png"/>
             </div>
-            <div class="face back-face">
+            <div class="face backFace">
                 <img src="Imagens/${arrayCartasTraseiras[i]}"/>
             </div>
         </div>`
     }
 }
 adicionarCartasFrontais();
+
+let cardsContainer = document.querySelector(".cardsContainer");
+let frontFace = document.querySelector(".frontFace");
+let backFace = document.querySelector(".backFace");
 
