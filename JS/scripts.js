2f81fb439171bc78e80db4ca5c0ed9d16ae422ea
GB-Navarro <devnavarro06@gmail.com>
2022-02-07 16:11:47 -0300

    Added data-identifiers

--- JS/scripts.js
+++ JS/scripts.js
@@ -15,24 +15,51 @@ do{
 function embaralharCartasTraseiras(){
     return Math.random() - 0.5; 
 }
-arrayCartasTraseiras.sort(embaralharCartasTraseiras)
+arrayCartasTraseiras.sort(embaralharCartasTraseiras);
 
 function adicionarCartasFrontais(){
     for(let i = 0; i < numeroDeCartas; i ++){
         main.innerHTML += 
-        `<div class="cardsContainer" onclick="virarCartas(children)">
-            <div class="face frontFace">
+        `<div class="cardsContainer" onclick="virarCartas(children)" data-identifier="card">
+            <div class="face frontFace" data-identifier="front-face">
                 <img src="Imagens/front.png"/>
             </div>
-            <div class="face backFace">
+            <div class="face backFace" data-identifier="back-face">
                 <img src="Imagens/${arrayCartasTraseiras[i]}"/>
             </div>
-        </div>`
+        </div>`;
     }
 }
 adicionarCartasFrontais();
 
 function virarCartas(cards){
-    cards[1].classList.toggle("back");
-    cards[0].classList.toggle("front");
+
+    let varControle = 0;
+    let arrayControle = [];
+    let j = 0;
+    
+   
+    let mainPovoado = document.querySelectorAll(".cardsContainer");
+
+    for(let i = 0; i < mainPovoado.length; i++){
+        if((((mainPovoado[i].childNodes[1]).classList.contains("front")) === true) && (((mainPovoado[i].childNodes[3]).classList.contains("back")) === true)){
+            arrayControle[j] = i; //Guardar em qual indice está a classe
+            varControle = varControle + 1;
+            j = j + 1;
+        }    
+    }
+    if(varControle < 2){
+        cards[1].classList.toggle("back");
+        cards[0].classList.toggle("front");
+    }
+    console.log(mainPovoado[arrayControle[0]]);
+    if((arrayControle[0] != undefined && arrayControle[1] != undefined) && (mainPovoado[arrayControle[0]] != mainPovoado[arrayControle[1]])){
+        (mainPovoado[arrayControle[0]].childNodes[1]).classList.remove("front");
+        (mainPovoado[arrayControle[0]].childNodes[3]).classList.remove("back");
+        (mainPovoado[arrayControle[1]].childNodes[1]).classList.remove("front");
+        (mainPovoado[arrayControle[1]].childNodes[3]).classList.remove("back");
+    }else if((arrayControle[0] != undefined && arrayControle[1] != undefined) && (mainPovoado[arrayControle[0]] === mainPovoado[arrayControle[1]])){
+        mainPovoado[arrayControle[0]].innerHTML = `teste`;
+        mainPovoado[arrayControle[0]].innerHTML = `teste`;            
+    }
 }
